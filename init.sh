14024ecf79db757d1fbdd77c3b2d429eedfdac3c
sunsijie <sunsijie@buaa.edu.cn>
2017-06-05 19:24:51 +0800

    Travis上不支持ln的-r选项，使用cp代替

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+if [ $TRAVIS ];then
+CP_FUNC="cp -f"
+else
+CP_FUNC="ln -srf"
+fi
+THEME_DIR=../themes/next
 git submodule update --init --recursive
 cd ./themes-customs
-find . -type f -exec ln -srf {} ../themes/next/{} \;
+find . -type f -exec rm ${THEME_DIR}/{} -f \; -exec ${CP_FUNC} {} ${THEME_DIR}/{} \;
 cd -
