695b426d547462e83d9bda6d6f6318e2ff22476c
Sijie.Sun <sunsijie@buaa.edu.cn>
2018-03-02 13:49:29 +0800

    resume

--- init.sh
+++ init.sh
@@ -2,10 +2,11 @@
 if [ $TRAVIS ];then
 CP_FUNC="cp -f"
 else
-CP_FUNC="ln -srf"
+CP_FUNC="ln -sf"
 fi
+CP_FUNC="cp -f"
 THEME_DIR=../themes/next
 git submodule update --init --recursive
 cd ./themes-customs
-find . -type f -exec rm ${THEME_DIR}/{} -f \; -exec ${CP_FUNC} {} ${THEME_DIR}/{} \;
+find . -type f -exec echo {} \; -exec rm -f ${THEME_DIR}/{} \; -exec ${CP_FUNC} {} ${THEME_DIR}/{} \;
 cd -
